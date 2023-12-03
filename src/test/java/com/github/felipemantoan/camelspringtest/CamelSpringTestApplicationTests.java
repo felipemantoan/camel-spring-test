8d9a12325e9761ac5973f376c910cfc048e04d28
Luis Felipe Mantoan <luisfelipemantoan@gmail.com>
2023-12-03 11:52:35 -0300

    Install spring e camel

---
+++ src/test/java/com/github/felipemantoan/camelspringtest/CamelSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.felipemantoan.camelspringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
