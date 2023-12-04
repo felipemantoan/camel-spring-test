cf8582201f2872dae0ad1e3482e203bdf8728fbf
Luis Felipe Mantoan <luisfelipemantoan@gmail.com>
2023-12-03 21:11:03 -0300

    Creates a new integration

--- src/main/java/com/github/felipemantoan/camelspringtest/route/MainRoute.java
+++ src/main/java/com/github/felipemantoan/camelspringtest/route/MainRoute.java
@@ -1,14 +1,29 @@
 package com.github.felipemantoan.camelspringtest.route;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MainRoute extends RouteBuilder{
 
+    private String backend = "https://webhook.site";
+
     @Override
     public void configure() throws Exception {
-        from("timer:foo").to("log:bar");
+
+        from("direct:foo").routeId("idFoo")
+            .removeHeaders("CamelHttp*")
+            .removeHeaders("*")
+            .setHeader(Exchange.HTTP_METHOD, constant("POST"))
+            .setHeader(Exchange.HTTP_BASE_URI, constant(backend))
+            .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+            .setBody(constant("{\"hello\": \"io\" }"))
+            .to(backend + "/3d388a93-df80-467c-af70-5ff15cc1e19d")
+            .removeHeaders("CamelHttp*")
+            .log("end ${body}")
+            .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200))
+            .end();
     }
     
 }
---
+++ src/main/java/com/github/felipemantoan/camelspringtest/route/RestRoute.java
@@ -0,0 +1,42 @@
+package com.github.felipemantoan.camelspringtest.route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestRoute extends RouteBuilder {
+
+    @Autowired
+    private Environment env;
+    
+    @Value("${camel.servlet.mapping.context-path}")
+    private String contextPath;
+
+    @Override
+    public void configure() throws Exception {
+
+        restConfiguration()
+            .component("servlet")
+            .bindingMode(RestBindingMode.json)
+            .dataFormatProperty("prettyPrint", "true")
+            .enableCORS(true)
+            .port(env.getProperty("server.port", "8080"))
+            .contextPath(contextPath.substring(0, contextPath.length() - 2))
+            // turn on openapi api-doc
+            .apiContextPath("/api-doc")
+            .apiProperty("api.title", "User API")
+            .apiProperty("api.version", "1.0.0");
+
+        rest("/test").description("User REST service")
+            .produces(MediaType.APPLICATION_JSON_VALUE)
+            .get().id("exposed")
+            .bindingMode(RestBindingMode.json)
+            .to("direct://foo");
+    }
+    
+}
