74fd9a17b1972ce10dab814bb202a415b690f068
Luis Felipe Mantoan <luisfelipemantoan@gmail.com>
2023-12-03 21:49:44 -0300

    Add marshal and unmarshal

--- src/main/java/com/github/felipemantoan/camelspringtest/route/MainRoute.java
+++ src/main/java/com/github/felipemantoan/camelspringtest/route/MainRoute.java
@@ -2,6 +2,8 @@ package com.github.felipemantoan.camelspringtest.route;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,17 +15,15 @@ public class MainRoute extends RouteBuilder{
     public void configure() throws Exception {
 
         from("direct:foo").routeId("idFoo")
-            .removeHeaders("CamelHttp*")
             .removeHeaders("*")
             .setHeader(Exchange.HTTP_METHOD, constant("POST"))
             .setHeader(Exchange.HTTP_BASE_URI, constant(backend))
-            .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
-            .setBody(constant("{\"hello\": \"io\" }"))
+            .setHeader(Exchange.CONTENT_TYPE, constant(MediaType.APPLICATION_JSON_VALUE))
+            .marshal().json(JsonLibrary.Jackson)
             .to(backend + "/3d388a93-df80-467c-af70-5ff15cc1e19d")
-            .removeHeaders("CamelHttp*")
-            .log("end ${body}")
-            .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200))
-            .end();
+            .unmarshal().json(JsonLibrary.Jackson)
+            .removeHeaders("*")
+            .log("end ${body}");
     }
     
 }
